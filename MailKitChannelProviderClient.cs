931fd7f8a18359d5999ab55ddc4a4740c2199e03
jeremy <517429+JeremyMelton@users.noreply.github.com>
2024-02-13 19:25:50 -0700

    initial commit

---
+++ MailKitChannelProviderClient.cs
@@ -0,0 +1,121 @@
+﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License")
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Transmitly.ChannelProvider;
+
+namespace Transmitly.MailKit
+{
+	internal class MailKitChannelProviderClient(MailKitConfigurationOptions optionObj) : ChannelProviderClient<IEmail>
+	{
+		private readonly MailKitConfigurationOptions _optionObj = Guard.AgainstNull(optionObj);
+
+		public override IReadOnlyCollection<string>? RegisteredEvents => [DeliveryReportEvent.Name.Delivered(), DeliveryReportEvent.Name.Dispatched(), DeliveryReportEvent.Name.Error()];
+
+		public override async Task<IReadOnlyCollection<IDispatchResult?>> DispatchAsync(IEmail email, IDispatchCommunicationContext communicationContext, CancellationToken cancellationToken)
+		{
+			Guard.AgainstNull(email);
+			Guard.AgainstNull(communicationContext);
+
+			var msg = new MimeMessage
+			{
+				MessageId = Guid.NewGuid().ToString("N")
+			};
+			msg.From.Add(email.From.ToMailboxAddress());
+			msg.To.AddRange(email.To!.Select(m => m.ToMailboxAddress()));
+			msg.Subject = email.Subject;
+
+			if (email.Bcc != null)
+				msg.Bcc.AddRange(email.Bcc.Select(x => x.ToMailboxAddress()));
+
+			if (email.Cc != null)
+				msg.Cc.AddRange(email.Cc.Select(x => x.ToMailboxAddress()));
+
+			if (email.ReplyTo != null)
+				msg.ReplyTo.AddRange(email.ReplyTo.Select(x => x.ToMailboxAddress()));
+
+			var body = new BodyBuilder()
+			{
+				HtmlBody = email.HtmlBody,
+				TextBody = email.TextBody
+			};
+
+			AddAttachments(body, email.Attachments, cancellationToken);
+
+			msg.Body = body.ToMessageBody();
+
+			var client = new SmtpClient();
+			await Connect(client, cancellationToken).ConfigureAwait(false);
+			string result = await Send(msg, client, cancellationToken).ConfigureAwait(false);
+			var commResult = new MailKitSendResult
+			{
+				IsDelivered = result.Equals("Ok", StringComparison.OrdinalIgnoreCase),
+				ResourceId = msg.MessageId,
+				MessageString = result
+			};
+			SendDeliveryReports(email, communicationContext, commResult);
+			return [commResult];
+		}
+
+		private void SendDeliveryReports(IEmail email, IDispatchCommunicationContext communicationContext, MailKitSendResult commResult)
+		{
+			Dispatched(communicationContext, email);
+			if (commResult.IsDelivered)
+				Delivered(communicationContext, email);
+			else
+				Error(communicationContext, email);
+		}
+
+		private static async Task<string> Send(MimeMessage msg, SmtpClient client, CancellationToken cancellationToken)
+		{
+			var result = await client.SendAsync(msg, cancellationToken).ConfigureAwait(false);
+			await client.DisconnectAsync(true, cancellationToken).ConfigureAwait(false);
+			return result;
+		}
+
+		private async Task Connect(SmtpClient client, CancellationToken cancellationToken)
+		{
+			var secureSocketOption = _optionObj.UseSsl ? SecureSocketOptions.StartTls : SecureSocketOptions.Auto;
+			await client.ConnectAsync(_optionObj.Host, _optionObj.Port ?? 0, secureSocketOption, cancellationToken).ConfigureAwait(false);
+			await client.AuthenticateAsync(_optionObj.UserName, _optionObj.Password, cancellationToken).ConfigureAwait(false);
+		}
+
+		private static void AddAttachments(BodyBuilder body, IReadOnlyCollection<IAttachment> attachments, CancellationToken cancellationToken)
+		{
+			foreach (var attachment in attachments)
+			{
+				var (mediaType, subType) = GetContentType(attachment.ContentType);
+				body.Attachments.Add(attachment.Name, attachment.ContentStream, new ContentType(mediaType, subType), cancellationToken);
+			}
+		}
+
+		private static (string mediaType, string subType) GetContentType(string? contentType)
+		{
+			(string, string) unknownContentType = ("application", "octet-stream");
+			if (string.IsNullOrWhiteSpace(contentType))
+				return unknownContentType;
+			var separateTypes = contentType!.Split('/');
+			if (separateTypes.Length != 2)
+				return unknownContentType;
+			return (separateTypes[0], separateTypes[1]);
+		}
+	}
+}
